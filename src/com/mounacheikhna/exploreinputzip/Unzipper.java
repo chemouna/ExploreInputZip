6581ce4699af1b8a078ee65a123943e8592fba89
Mouna Cheikhna <cheikhnamouna@gmail.com>
2017-12-15 18:13:43 +0000

    Unzipper : another version..

--- src/com/mounacheikhna/exploreinputzip/Unzipper.java
+++ src/com/mounacheikhna/exploreinputzip/Unzipper.java
@@ -20,7 +20,7 @@ public class Unzipper {
                 if(ze.getName().endsWith(".txt")) {
                     FileOutputStream fout = new FileOutputStream(ze.getName());
                     InputStream in = zf.getInputStream(ze);
-                    copy(in, fout);
+                    Copier.copy(in, fout);
                     in.close();
                     fout.close();
                 }
@@ -28,22 +28,6 @@ public class Unzipper {
         } catch (IOException e) {
             System.err.println(e);
             e.printStackTrace();
-        }
-    }
-
-    private static void copy(InputStream in, OutputStream out)
-            throws IOException {
-        // Do not allow other threads to read from the input
-        // or write to the output while copying is taking place
-        synchronized (in) {
-            synchronized (out) {
-                byte[] buffer = new byte[256];
-                while (true) {
-                    int bytesRead = in.read(buffer);
-                    if (bytesRead == -1) break;
-                    out.write(buffer, 0, bytesRead);
-                }
-            }
         }
     }
 
---
+++ src/com/mounacheikhna/exploreinputzip/Unzipper2.java
@@ -0,0 +1,30 @@
+package com.mounacheikhna.exploreinputzip;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+public class Unzipper2 {
+
+    public static void main(String[] args) {
+        try {
+            FileInputStream fin = new FileInputStream("resources/simple.zip");
+            ZipInputStream zin = new ZipInputStream(fin);
+            ZipEntry ze;
+            while ((ze = zin.getNextEntry()) != null) {
+                System.out.println("Unzipping " + ze.getName());
+                FileOutputStream fout = new FileOutputStream(ze.getName());
+                Copier.copy(zin, fout);
+                zin.closeEntry();
+                fout.close();
+            }
+            zin.close();
+        } catch (IOException e) {
+            System.err.println(e);
+            e.printStackTrace();
+        }
+    }
+
+}
